550bf16b275b7aafe0652a89efe48fb5f1c2e556
iraqis <62575693+iraqis1@users.noreply.github.com>
2020-05-05 01:56:44 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome to source iraqis Developers
+
+[![VHHHHH](https://t.me/vhhhhh)](https://t.me/vhhhhh)
+
+[![@HHMHHH](https://t.me/HHMHHH)](https://t.me/HHMHHH)  
+
+[![@cCcYo](https://t.me/cCcYo)](https://t.me/cCcYo) 
+
+[![Developers channel](https://T.me/cqccqq)](https://T.me/cqccqq)
+
+[![Deploy To Heroku](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy) 
---
+++ telesetup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# (c) https://t.me/TelethonChat/37677
+# This Source Code Form is subject to the terms of the GNU
+# General Public License, v.3.0. If a copy of the GPL was not distributed with this
+# file, You can obtain one at https://www.gnu.org/licenses/gpl-3.0.en.html.
+
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+
+print("""Welcome to source iraqis Developers
+t.me/vhhhhh
+t.me/HHMHHH
+t.me/CcCYo""")
+APP_ID = int(input("Enter APP ID here: "))
+API_HASH = input("Enter API HASH here: ")
+
+with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
+    print(client.session.save())
